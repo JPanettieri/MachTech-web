97a38bf983b4fd9874b22d37c93e1f9b506a2006
JPanettieri <joshua@mach-tech.com.au>
2023-03-27 22:49:10 +1100

    Update pages and css

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+        # Pages
+    path('', views.index),
+    path('about-us/', views.abouts_us, name='about_us'),
+    path('contact-us/', views.contact_us, name='contact_us'),
+    path('landing-freelancer/', views.landing_freelancer, name='landing_freelancer'),
+    path('blank/', views.blank_page, name='blank'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,17 @@ def index(request):
 
     # Page from the theme 
     return render(request, 'pages/index.html')
+
+# Pages
+
+def abouts_us(request):
+  return render(request, 'pages/about.html')
+
+def contact_us(request):
+  return render(request, 'pages/contact.html')
+
+def landing_freelancer(request):
+  return render(request, 'pages/landing-freelancer.html')
+
+def blank_page(request):
+  return render(request, 'pages/blank.html')
