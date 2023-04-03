a3a9f37d92dc104ecdd2f48e8d7fc8af8cfdf82e
JPanettieri <joshua@mach-tech.com.au>
2023-04-04 07:09:46 +1000

    expose port

--- core/settings.py
+++ core/settings.py
@@ -31,8 +31,13 @@ if not SECRET_KEY:
 # Render Deployment Code
 DEBUG = 'RENDER' not in os.environ
 
-# Docker HOST
-ALLOWED_HOSTS = ['0.0.0.0:8080 ']
+PORT = int(os.environ.get("PORT", 8000))
+ALLOWED_HOSTS = ['*']
+
+# Start the development server
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:{}'.format(PORT)])
 
 # Add here your deployment HOSTS
 CSRF_TRUSTED_ORIGINS = ['0.0.0.0:8080', 'www.zenixion.dev']
