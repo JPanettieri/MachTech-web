f34e73a97af369dff6b01480abe83074718b1df1
JPanettieri <joshua@mach-tech.com.au>
2023-04-04 08:01:36 +1000

    gunicorn update

--- gunicorn-cfg.py
+++ gunicorn-cfg.py
@@ -2,9 +2,10 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import multiprocessing
 
-bind = '0.0.0.0:5005'
-workers = 1
+bind = '0.0.0.0:' + str(5005)
+workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '-'
 loglevel = 'debug'
 capture_output = True
