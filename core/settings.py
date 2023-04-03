919bd177ada9a1c85c239c30233f6c41992df0ae
JPanettieri <joshua@mach-tech.com.au>
2023-04-04 07:23:41 +1000

    updated trusted origins

--- core/settings.py
+++ core/settings.py
@@ -31,10 +31,19 @@ if not SECRET_KEY:
 # Render Deployment Code
 DEBUG = os.environ.get('DEBUG')
 
+PORT = int(os.environ.get("PORT", 8000))
 ALLOWED_HOSTS = ['*']
 
+# Start the development server
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:{}'.format(PORT)])
+
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['0.0.0.0:8080', 'www.zenixion.dev']
+CSRF_TRUSTED_ORIGINS = [
+    'http://0.0.0.0:8080',
+    'https://www.zenixion.dev',
+]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
